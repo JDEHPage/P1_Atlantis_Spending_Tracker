4273b830ff7a6f714ccba7b22086b58cdc173956
John Page <jdehpage@gmail.com>
2019-04-06 19:08:15 +0100

    Update

--- App/app.rb
+++ App/app.rb
@@ -1,5 +1,5 @@
 require( 'sinatra' )
-require( 'sinatra/contrib/all' )
+# require( 'sinatra/contrib/all' )
 require_relative('controllers/merchant_controller')
 require_relative('controllers/tag_controller')
 require_relative('controllers/transaction_controller')
